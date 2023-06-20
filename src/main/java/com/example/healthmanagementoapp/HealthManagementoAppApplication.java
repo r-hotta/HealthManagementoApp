7a073eeddb61bedf503fbfcdebd0d649ddb114c4
r-hotta <r-hotta@kokusaiexpress.com>
2023-06-20 13:16:51 +0900

    second commit

---
+++ src/main/java/com/example/healthmanagementoapp/HealthManagementoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthmanagementoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthManagementoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthManagementoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/healthmanagementoapp/HealthManagementoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthmanagementoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthManagementoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
